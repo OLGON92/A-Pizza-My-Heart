83cf8e67497d3393bd73e3336f570baeff7d2ff0
OLGON92 <ogon318@gmail.com>
2022-09-23 13:31:42 -0700

    add a pizza protoype with 2 map objects.

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,20 @@ function Pizza(size, toppings) {
   this.size = size;
   this.toppings= toppings;
 }
+Pizza.prototype.cost = function() {
+  let toppingsTotal = 0;
+  const size = new Map([
+    ["Small", 8],
+    ["Medium", 12],
+    ["Large", 16]
+  ]);
+  const toppings = new Map([
+    ["Pepperoni", 4],
+    ["Sausage", 4],
+    ["Saitan", 5],
+    ["Mushrooms", 3],
+    ["Peppers", 3],
+    ["Onions", 1]
+  ]);
+  
+}

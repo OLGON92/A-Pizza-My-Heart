d43aec6646906ae6d48b29dfcae75c4aa4f472fb
OLGON92 <ogon318@gmail.com>
2022-09-24 22:26:45 -0700

    add the start of UI Logic.

--- js/scripts.js
+++ js/scripts.js
@@ -7,17 +7,17 @@ function Pizza(size, toppings) {
 Pizza.prototype.cost = function() {
   let toppingsTotal = 0;
   const size = new Map([
-    ["Small", 8],
-    ["Medium", 12],
-    ["Large", 16]
+    ["Small", 8.00],
+    ["Medium", 12.00],
+    ["Large", 16.00]
   ]);
   const toppings = new Map([
-    ["Pepperoni", 4],
-    ["Sausage", 4],
-    ["Saitan", 5],
-    ["Mushrooms", 3],
-    ["Peppers", 3],
-    ["Onions", 1]
+    ["Pepperoni", 4.00],
+    ["Sausage", 4.00],
+    ["Saitan", 5.00],
+    ["Mushrooms", 3.00],
+    ["Peppers", 3.00],
+    ["Onions", 1.00]
   ]);
   this.toppings.forEach(function(element) {
     toppingsTotal += toppings.get(element)
@@ -25,3 +25,18 @@ Pizza.prototype.cost = function() {
   price = size.get(this.size) + toppingsTotal;
   return price;
 }
+
+//UI Logic
+function handleFormSubmission(event) {
+  event.preventDefault();
+  let pizzaSize = document.querySelector("input:radio[name=pizzaSize]:checked").value;
+  let pizzaToppings = [];
+  document.querySelector("input:checkbox[name=Toppings]:checked").each(function(){
+    pizzaToppings.push(this)
+  });
+  newPizza = new Pizza(pizzaSize, pizzaToppings);
+  
+}
+window.addEventListener("load", function() {
+  this.document.querySelector("form#new-za"),this.addEventListener("submit", handleFormSubmission);
+});
